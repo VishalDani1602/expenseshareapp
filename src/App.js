10640fa38a0bcbc38d2fc7652d373ad65b156c19
Vishal Dani <vishal.dani123@gmail.com>
2023-10-06 18:40:55 -0700

    Signup Functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
 import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes > 
           <Route path="/login" element={ <Login />}/>
+          <Route path="/signup" element={<Signup />}/>
           </Routes > 
       </div>
     </Router>
---
+++ src/Signup.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './Signup.css';
+import { Link,useNavigate } from 'react-router-dom';
+import axios from './axios';
+
+
+function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    
+      if (password !== confirmPassword) {
+        console.error('Passwords do not match');
+        return;
+      }
+
+      axios.post('/users/create', { "email":email, "password": password }).then(()=>{
+        navigate('/login');
+      }).catch((error)=>{
+        window.alert('Registration failed', error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSignup}>
+        <h2>Signup</h2>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Signup</button>
+      </form>
+      <p>Already have an account? <Link to="/login">Login</Link></p> {/* Link to the login page */}
+
+    </div>
+  );
+}
+
+export default Signup;
